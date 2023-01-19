09172bbaf0961ee8b8a6d12f28855fff7422eae7
merajuni7ed <merajmusavi1243m@gmail.com>
2023-01-19 13:01:39 +0330

    Initial commit

---
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firstkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this,"meraj",Toast.LENGTH_SHORT).show()
+
+
+    }
+
+}
